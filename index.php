d6d693a5475c418acd4b8158ff2961e56c952e69
kebron <kebron@4e0f6229-4713-0410-acaf-c9ad0a51243b>
2006-06-26 10:20:43 +0000

    add menu to manage/index.html and register/index.html

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<html>
-<head></head>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+<title>YaCy-Newsletter</title>
 <body>
-<a href="register/">Register a new account</a><br>
-<a href="manage/">Manage your account</a><br>
-<small><?php include 'functions.php'; show_version();?></small>
+<a href="register/index.html">Register a new account</a> | <a href="manage/index.html">Manage your account</a>
+<small><?php include 'functions.php'; show_version();?></small><br />
+<hr noshade="noshade" />
 </body>
 </html>
