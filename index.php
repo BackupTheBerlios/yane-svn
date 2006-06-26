081a7c3cdc989878798565202cb585834476ccb9
kebron <kebron@4e0f6229-4713-0410-acaf-c9ad0a51243b>
2006-06-26 13:15:38 +0000

    add css

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; ?>
+ <?php echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
 <title>YaCy-Newsletter</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</link>
 <body>
-<a href="register/index.html">Register a new account</a> | <a href="manage/index.html">Manage your account</a>
-<small><?php include 'functions.php'; show_version();?></small><br />
-<hr noshade="noshade" />
+<table class="navigation">
+ <tr>
+  <td style="width:70px"rowspan="2" class="white"><img src="yacy.png" /></td>
+  <td style="width:200px" class="blue"><a href="register/index.html">Register a new account</td>
+  <td style="width:200px" class="blue"><a href="manage/index.html">Manage your account</a></td>
+  <td class="blue"></td>
+ </tr>
+ <tr><td style="height:20px" class="white"></td></tr>
+</table>
+<br /><br /><br /><br /><br /><br />
+ <p id="Fusszeile"><small><?php include 'functions.php'; show_version();?></small><br /></p>
+
 </body>
 </html>
